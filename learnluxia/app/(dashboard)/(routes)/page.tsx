e518769a5b0ea92cc8bd3c4f791f027dd97e78e0
h6myman <abhi32gbram@gmail.com>
2023-09-22 17:55:38 +0530

    Setup Authentication and Routes

---
+++ learnluxia/app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import { Button } from "@/components/ui/button"
+import { UserButton } from '@clerk/nextjs'
+
+
+export default function Home() {
+  return (
+    
+    <div>
+      <UserButton afterSignOutUrl='/'/>
+
+      
+      This is a well protected page ..VARY NIVE 
+    </div>
+   )
+}
