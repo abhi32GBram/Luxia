8f53e9573b22ffae4193d88e3f716867e18b6ae9
h6myman <abhiram.suradkar@gmail.com>
2023-11-12 04:26:28 +0530

    Added Video Player from MUX API

--- learnluxia/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
+++ learnluxia/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
@@ -1,100 +1,100 @@
+// This line indicates that the code should run only on the client-side and not during server-side rendering.
 'use client'
+
+// Importing necessary React and third-party libraries
 import React from 'react'
-
 import * as z from "zod"
-
-
-
 import { useState } from 'react'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
-
-
-
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
-
 import { Ghost, ImageIcon, Pencil, PlusCircle, Video } from 'lucide-react'
-
 import toast from 'react-hot-toast'
-
-
-
-import { Course, MuxData, Chapter} from '@prisma/client'
+import MuxPlayer from '@mux/mux-player-react'
+import { Course, MuxData, Chapter } from '@prisma/client'
 import Image from 'next/image'
-
 import { FileUpload } from '@/components/file-upload'
 
-
+// Defining the props for the ChapterVideoForm component
 interface ChapterVideoProps {
-
-    initialData: Chapter & {muxData?: MuxData | null }
+    initialData: Chapter & { muxData?: MuxData | null }
     courseId: string
-    chapterId : string 
+    chapterId: string
 }
 
+// Schema for form validation using Zod library
 const formSchema = z.object({
     videoUrl: z.string().min(1)
 })
 
+// The ChapterVideoForm component
 const ChapterVideoForm = ({
-    initialData, courseId,chapterId
+    initialData, courseId, chapterId
 }: ChapterVideoProps) => {
-
+    // State to manage edit mode
     const [isEditing, setisEditing] = useState(false)
 
+    // Function to toggle edit mode
     const toggleEdit = () => {
         setisEditing((current) => !current)
     }
 
+    // Hook to use Next.js router for navigation
     const router = useRouter()
 
+    // Function to handle form submission
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
+            // Sending PATCH request to update chapter video
             await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}`, values)
+            // Display success message
             toast.success("Chapter Update Successfully ")
+            // Exit edit mode and refresh the page
             toggleEdit()
             router.refresh()
         } catch {
+            // Display error message on failure
             toast.error("Something Went Wrong ")
         }
     }
-    return (
 
+    // Rendering the component
+    return (
         <div className='mt-6 border bg-slate-100 rounded-md p-4'>
             <div className='font-medium flex items-center justify-between'>
                 Chapter Video
                 <Button onClick={toggleEdit} variant="ghost">
-                    {isEditing && (
+                    {isEditing ? (
                         <>Cancel</>
-                    )}
-                    {!isEditing && !initialData.videoUrl && (
+                    ) : !initialData.videoUrl ? (
                         <>
                             <PlusCircle className='h-4 w-4 mr-2' />
                             Add a Video
                         </>
-                    )}
-                    {!isEditing && initialData.videoUrl && (
+                    ) : (
                         <>
                             <Pencil className='h-4 w-4 mr-2' />
                             Edit the Video
                         </>
                     )}
-
                 </Button>
             </div>
             {!isEditing && (
                 !initialData.videoUrl ? (
+                    // Display placeholder if no video URL is present
                     <div className='flex items-center justify-center h-60 bg-slate-200 rounded-md'>
                         <Video className='h-10 w-10 text-slate-500' />
                     </div>
                 ) : (
+                    // Display the video player if video URL exists
                     <div className='relative aspect-video mt-2'>
-                        Video Uploaded Successfully ! 
+                        <MuxPlayer playbackId={initialData?.muxData?.playbackId || ''} />
                     </div>
                 )
             )}
             {isEditing && (
+                // File upload component to upload a new video
                 <div>
                     <FileUpload endpoint='chapterVideo' onChange={(url) => {
                         if (url) {
@@ -103,33 +103,15 @@ const ChapterVideoForm = ({
                     }}
                     />
                     <div className='text-xs text-muted-foreground mt-4'>
-                        Upload a Video for this Chapter 
+                        Upload a Video for this Chapter
                     </div>
-
                 </div>
-                // <Form {...form}>
-                //     <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4 mt-4'>
-                //         <FormField control={form.control} name='description' render={({field}) => (
-                //             <FormItem>
-                //                 <FormControl>
-                //                     <Textarea disabled={isSubmitting} placeholder="'Eg, : 'This Course will be About....'" {...field}/>
-
-                //                 </FormControl>
-                //                 <FormMessage />
-                //             </FormItem>
-                //         )} />
-                //         <div className='flex items-center gap-x-2'>
-                //             <Button disabled={!isValid || isSubmitting} type='submit'>
-                //                 Save 
-                //             </Button>
-                //         </div>
-                //     </form>
-                // </Form>
             )}
             {initialData.videoUrl && !isEditing && (
+                // Display a note about video processing time
                 <div className='text-xs text-muted-foreground mt-2'>
                     Videos can take a few Minutes to Process...
-                    Refresh the Page if Video dosen&apos;t Appear 
+                    Refresh the Page if Video dosen&apos;t Appear
                 </div>
             )}
         </div>
@@ -137,5 +119,3 @@ const ChapterVideoForm = ({
 }
 
 export default ChapterVideoForm
-
-// 5 37 10
--- learnluxia/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ learnluxia/app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,28 +1,47 @@
-import { db } from "@/lib/db";
-import { auth } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
+// Importing necessary modules and libraries
+import { db } from "@/lib/db"; // Database access module
+import { auth } from "@clerk/nextjs"; // Authentication module from Clerk
+import { NextResponse } from "next/server"; // Response utility from Next.js
 
+// Importing the Mux SDK for video asset management
+import Mux from '@mux/mux-node';
+const { Video } = new Mux(
+    process.env.MUX_TOKEN_ID!, // Mux API token ID
+    process.env.MUX_TOKEN_SECRET!, // Mux API token secret
+);
+
+// The PATCH function to update chapter information
 export async function PATCH(
-    req: Request,
-    { params }: { params: { courseId: string; chapterId: string } }
+    req: Request, // The request object
+    { params }: { params: { courseId: string; chapterId: string } } // Destructuring params from the request
 ) {
     try {
-        const { userId } = auth()
-        const { isPublished, ...values } = await req.json()
+        // Getting the user ID from the auth module
+        const { userId } = auth();
 
+        // Extracting data from the request body
+        const { isPublished, ...values } = await req.json();
+
+        // Check for authorized user
         if (!userId) {
-            return new NextResponse("Unauthorized Access", { status: 401 })
+            // Return 401 Unauthorized if no user ID is found
+            return new NextResponse("Unauthorized Access", { status: 401 });
         }
+
+        // Check if the user is the owner of the course
         const courseOwner = await db.course.findUnique({
             where: {
                 id: params.courseId,
                 userId
             }
-        })
+        });
 
+        // Return 401 Unauthorized if the user is not the course owner
         if (!courseOwner) {
-            return new NextResponse("Unauthorized Access", { status: 401 })
+            return new NextResponse("Unauthorized Access", { status: 401 });
         }
+
+        // Update the chapter with the provided values
         const chapter = await db.chapter.update({
             where: {
                 id: params.chapterId,
@@ -31,14 +50,49 @@ export async function PATCH(
             data: {
                 ...values
             }
-        })
+        });
 
-        // TO DO : HANDLE THE VIDEO UPLOAD THING 
+        // If a video URL is provided in the update
+        if (values.videoUrl) {
+            // Look for existing Mux data for the chapter
+            const existingMuxData = await db.muxData.findFirst({
+                where: {
+                    chapterId: params.chapterId
+                }
+            });
 
-        
-        return NextResponse.json(chapter)
+            // If Mux data exists, delete the old video asset and data
+            if (existingMuxData) {
+                await Video.Assets.del(existingMuxData.assetId);
+                await db.muxData.delete({
+                    where: {
+                        id: existingMuxData.id
+                    }
+                });
+            }
+
+            // Create a new Mux asset with the provided video URL
+            const asset = await Video.Assets.create({
+                input: values.videoUrl,
+                playback_policy: "public",
+                test: false
+            });
+
+            // Create a new Mux data entry in the database
+            await db.muxData.create({
+                data: {
+                    chapterId: params.chapterId,
+                    assetId: asset.id,
+                    playbackId: asset.playback_ids?.[0]?.id ?? "defaultPlaybackId", // Safeguard against undefined playback ID
+                }
+            });
+        }
+
+        // Return the updated chapter data as a response
+        return NextResponse.json(chapter);
     } catch (error) {
-        console.log("[COURSES_CHAPTER_ID]", error)
-        return new NextResponse("Internal Error Occured ", { status: 500 })
+        // Log the error and return a 500 Internal Server Error response
+        console.log("[COURSES_CHAPTER_ID]", error);
+        return new NextResponse("Internal Error Occured ", { status: 500 });
     }
 }
